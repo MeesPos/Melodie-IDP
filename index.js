71a4141480856dc01f915cb528321894f6448912
MeesPos <meespostma_@hotmail.com>
2020-12-09 15:09:59 +0100

    Klaar om te starten

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-import Handsfree from './node_modules/handsfree/src/handsfree.js'
-const handsfree = new Handsfree({
-  assetsPath: './assets',
-  face: true
-})
-
-handsfree.start()
+import * as Tone from './node_modules/tone/build/Tone.js'
