7615ea1807c7c77d9756748092c72e42715c86ad
MeesPos <meespostma_@hotmail.com>
2021-01-04 21:31:37 +0100

    Carousel werkt, middelste card moet nog gefixd worden

--- js/getPages.js
+++ js/getPages.js
@@ -1,19 +1,38 @@
 function loadStart() {
 
     let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("page-content").innerHTML =
-                this.responseText;
-        }
-    };
+            document.getElementById("page-content").innerHTML = this.responseText;
+            $('.owl-carousel').owlCarousel({
+                loop: true,
+                nav: true,
+                autoplay: true,
+                autoplayTimeout: 5000,
+                autoplayHoverPause: true,
+                items: 3,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    1200: {
+                        items: 3
+                    },
+                }
+            });
+        };
+    }
     xhttp.open("GET", "startPage.html", true);
     xhttp.send();
 }
+
 function loadMelodyMaker() {
 
     let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("page-content").innerHTML =
                 this.responseText;
