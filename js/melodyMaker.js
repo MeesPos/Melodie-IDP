5bb22ba9a49087720f022440853612207a2103b3
Cornell van der Straaten <30183@ma-web.nl>
2021-01-06 19:51:21 +0100

    Music can be made

--- js/melodyMaker.js
+++ js/melodyMaker.js
@@ -1,9 +1,45 @@
+ //create a synth and connect it to the main output (your speakers)
+let synth = new Tone.Synth().toDestination();
+function playTone(note) {
+    //play a middle 'C' for the duration of an 8th note
+    console.log(note);
+    synth.triggerAttackRelease(note, "32n");
+    tijdlijn.voegToonToe(note);
+}
 
-    //create a synth and connect it to the main output (your speakers)
-    let synth = new Tone.Synth().toDestination();
-    function playTone(note) {
-        //play a middle 'C' for the duration of an 8th note
-        console.log(note);
-        synth.triggerAttackRelease(note, "8n");
-        
-    }
+/**
+ * Stappenplan Tijdlijn
+ * - Bij toevoeging van noot, stop noot in array plus verhoog de tijdlijn-tijd met een halve seconde (1/8 note)
+ */
+
+let tijdlijn = {
+    duratie: 0,
+    alleTonen: [],        
+    
+
+    voegToonToe(toon){
+        this.duratie += 500;
+        this.alleTonen.push(toon);
+        this.refreshPage();
+    },
+
+    refreshPage(){
+        let html = `<p>Tijdlijn dingen enzo</p>`;    
+        document.getElementById('timelijn').innerHTML = html;
+        document.getElementById('duratie').innerHTML = this.duratie + ' seconde(n)';
+    },
+
+    play(){
+        let seq = new Tone.Sequence((time, note) => {
+            synth.triggerAttackRelease(note, 0.1, time);
+            console.log(time, note)
+        }, this.alleTonen, '8n' ).start(0);
+        Tone.Transport.start();
+        setInterval(()=>{Tone.Transport.stop()}, this.duratie)
+    },
+
+}
+
+
+
+
