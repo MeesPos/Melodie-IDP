ad685d5f2423bd059b6451a886934d917670429f
MeesPos <meespostma_@hotmail.com>
2021-01-07 13:34:00 +0100

    Tijdlijn af

--- js/melodyMaker.js
+++ js/melodyMaker.js
@@ -1,49 +1,60 @@
- //create a synth and connect it to the main output (your speakers)
+//create a synth and connect it to the main output (your speakers)
 let synth = new Tone.Synth().toDestination();
+
 function playTone(note) {
     //play a middle 'C' for the duration of an 8th note
     console.log(note);
     synth.triggerAttackRelease(note, "8n");
     tijdlijn.voegToonToe(note);
+    document.getElementById('timelijn').innerHTML += `<div class="tone ${note}"></div>`
 }
 
 /**
  * Stappenplan Tijdlijn
  * - Bij toevoeging van noot, stop noot in array plus verhoog de tijdlijn-tijd met een halve seconde (1/8 note)
  */
-
 let tijdlijn = {
     duratie: 0,
-    alleTonen: [],        
-    
+    alleTonen: [],
 
-    voegToonToe(toon){
-        this.duratie += 500;
+    voegToonToe(toon) {
+        this.duratie += 0.5;
         this.alleTonen.push(toon);
         this.refreshPage();
     },
 
-    refreshPage(){
-        let html = `<p>Tijdlijn dingen enzo</p>`;    
-        document.getElementById('timelijn').innerHTML = html;
+    refreshPage() {
         document.getElementById('duratie').innerHTML = this.duratie + ' seconde(n)';
     },
 
-    play(){
+    play() {
         time = 0
         let jonguh = 0;
         new Tone.Sequence((time, note) => {
             console.log(time)
             synth.triggerAttackRelease(note, .25, time);
             console.log(time, note);
-            jonguh += 0.000001;
         }, this.alleTonen, '4n').start(0);
         Tone.Transport.start();
-        setInterval(()=>{Tone.Transport.stop()}, this.alleTonen.length * 1000 * .5 - 100);
+        setInterval(() => { Tone.Transport.stop() }, this.alleTonen.length * 1000 * .5 - 100);
     },
 }
 
-
-
-
-
+function animatethis(targetElement, speed) {
+    var scrollWidth = $(targetElement).get(0).scrollWidth;
+    var clientWidth = $(targetElement).get(0).clientWidth;
+    $(targetElement).animate({ scrollLeft: scrollWidth - clientWidth },
+    {
+        duration: speed,
+        complete: function () {
+            targetElement.animate({ scrollLeft: 0 },
+            {
+                duration: speed,
+                complete: function () {
+                    animatethis(targetElement, speed);
+                }
+            });
+        }
+    });
+};
+animatethis($('#timelijn'), 5000);
