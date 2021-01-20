4773fb74de4846baf715526c80feccc62114d93f
Cornell van der Straaten <30183@ma-web.nl>
2021-01-20 21:29:46 +0100

    Delete note done

--- js/melodyMaker.js
+++ js/melodyMaker.js
@@ -1,12 +1,18 @@
 //create a synth and connect it to the main output (your speakers)
 let synth = new Tone.Synth().toDestination();
-
+let toonIDcount = 0;
 function playTone(note) {
-    //play a middle 'C' for the duration of an 8th note
-    console.log(note);
+    // Play the given tone for the duration of an 8th note
     synth.triggerAttackRelease(note, "8n");
-    tijdlijn.voegToonToe(note);
-    document.getElementById('timelijn').innerHTML += `<div class="tone ${note}"></div>`
+
+    // Add tone to allTone array in tijdlijn object
+    tijdlijn.voegToonToe(note, toonIDcount);
+
+    // Generate HTML to set in timeline
+    document.getElementById('timelijn').innerHTML += `<div class="tone ${note}" onclick="tijdlijn.deleteTone(${toonIDcount})" id="tone${toonIDcount}" ></div>`;
+
+    // Generate ID on amount of notes added
+    toonIDcount += 1;
 }
 /**
  * Stappenplan Tijdlijn
@@ -14,18 +20,33 @@ function playTone(note) {
  */
 let tijdlijn = {
     duratie: 0,
-    alleTonen: [],
+    toonOpslag: [],
 
-    voegToonToe(toon) {
+    voegToonToe(toon, toonID) {
         if (this.duratie < 60) {
             // Als duratie minder dan of gelijk is aan 60 sec
+            // Per toon, melodie duratie plus halve seconde
             this.duratie += 0.5;
-            this.alleTonen.push(toon);
+            this.toonOpslag.push({id: toonID, tone: toon})
             this.refreshPage();
         } else {
             alert('Maximale lengte bereikt.')
         }
         
+    },
+    
+    deleteTone(toDeleteID) {
+        // Removes the note from the timeline from DOM
+        let element = document.getElementById("tone" + toDeleteID);
+        element.parentNode.removeChild(element);
+
+        // Removes note from array
+        // we have an array of objects, we want to remove one object using only the id property
+        // get index of object with id toDeleteID
+        let removeIndex = this.toonOpslag.map(function(toonObject) { return toonObject.id; }).indexOf(toDeleteID);
+
+        // remove object
+        this.toonOpslag.splice(removeIndex, 1);
     },
 
     refreshPage() {
@@ -33,13 +54,21 @@ let tijdlijn = {
     },
 
     play() {
+
+        // Make array with all tones to play
+        let alleTonen = this.toonOpslag.map(a => a.tone);
+
         new Tone.Sequence((time, note) => {
-            console.log(time)
             synth.triggerAttackRelease(note, .25, time);
-            console.log(time, note);
-        }, this.alleTonen, '4n').start(0);
+        }, alleTonen, '4n').start(0);
+
+        // Play the music
         Tone.Transport.start();
-        setInterval(() => { tijdlijn.stop() }, this.alleTonen.length * 1000 * .5 - 100);
+        Tone.Transport.debug = true;
+        // After time elapsed stop playing
+        setInterval(() => { tijdlijn.stop() }, this.toonOpslag.length * 1000 * .5 - 100);
+
+        // Change playbutton
         document.getElementById('button').classList.remove('fas');
         document.getElementById('button').classList.remove('fa-play');
         document.getElementById('button').classList.add('far');
@@ -49,7 +78,10 @@ let tijdlijn = {
     },
 
     stop() {
+        // Stop music mid playing
         Tone.Transport.stop();
+
+        // Change playbutton
         document.getElementById('button').classList.remove('far');
         document.getElementById('button').classList.remove('fa-square');
         document.getElementById('button').classList.add('fas');
